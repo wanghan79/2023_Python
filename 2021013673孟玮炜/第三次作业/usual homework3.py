13064f885106d34f58e4ffd310def113c7721457
Mengweiwei0123 <109775304+Mengweiwei0123@users.noreply.github.com>
2023-06-21 09:34:24 +0800

    Add files via upload

---
+++ 2021013673孟玮炜/第三次作业/usual homework3.py
@@ -0,0 +1,44 @@
+import requests
+# from collections import Iterable,Iterator
+from collections.abc import Iterable, Iterator
+
+cityDictionary = {
+    "长春": "101060101",
+    "成都": "101270101",
+    "厦门": "101230201",
+    "广州": "101280101",
+    "上海":"101020100",
+    "石家庄":"101090101",
+    "郑州":"101180101",
+    "杭州":"101210101"
+}
+
+
+class WeatherIterator(Iterator):
+    def __init__(self, cities):
+        self.cities = cities
+        self.index = 0
+
+    def __next__(self):
+        if self.index == len(self.cities):
+            raise StopIteration
+        city = self.cities[self.index]
+        self.index += 1
+        return self.getweather(city)
+
+    def getweather(self, city):
+        cityCode = cityDictionary[city]
+        r = requests.get(u'http://t.weather.sojson.com/api/weather/city/' + cityCode)
+        data = r.json()['data']['forecast'][0]
+        return ' %s %s %s市天气%s %s %s %s ' \
+            % (data['ymd'],data['week'],city,data['type'],data['low'],data['high'],data['notice'])
+class WeatherIterable(Iterable):
+    def __init__(self, cities):
+        self.cities = cities
+
+    def __iter__(self):
+        return WeatherIterator(self.cities)
+
+
+for x in WeatherIterable(["长春", "郑州", "成都", "广州"]):
+    print(x)
