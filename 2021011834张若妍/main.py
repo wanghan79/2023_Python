d6ffa30c8c0d204ef947431b083e5129bddb1d2b
hei0v0 <115199879+hei0v0@users.noreply.github.com>
2023-06-24 23:26:06 +0800

    Add files via upload

---
+++ 2021011834张若妍/main.py
@@ -0,0 +1,69 @@
+from work1 import dataSimpling
+from work2 import decorator
+from work3 import weather
+
+if __name__ == "__main__":
+    while(True):
+        work_id = input("输入作业编号(输入1，2，3，输入exit退出)：")
+        if work_id == 'exit':
+            break
+        elif work_id == "1":
+            para = {
+                "num": 10,
+                "struct": (
+                    {
+                        'datatype': "int",
+                        'datarange': [1, 100]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    },
+                    {
+                        'datatype': "float",
+                        'datarange': [1.0, 100.0]
+                    },
+                    {
+                        'datatype': "str",
+                        'datarange': "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                        'len': 8
+                    }
+                )
+            }
+            dataSimpling.dataSimpling(para)
+        elif work_id == "2":
+            para = {
+                "num": 10,
+                "struct": (
+                    {
+                        'datatype': "int",
+                        'datarange': [1, 100]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    },
+                    {
+                        'datatype': "int",
+                        'datarange': [100, 250]
+                    }
+                )
+            }
+            decorator.decorator(para)
+        elif work_id == "3":
+            weather.weather()
+        else:
+            print("输入错误，请重新输入")
+        print()
