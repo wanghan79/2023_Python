8f445c9a69c91139ace9cc6a125da7a3dae6ae49
Ulquiorra-hzn <128786865+Ulquiorra-hzn@users.noreply.github.com>
2023-06-26 13:00:33 +0800

    Update main.py

--- 2021010067韩振南/finalwork/main.py
+++ 2021010067韩振南/finalwork/main.py
@@ -3,7 +3,7 @@
 :Date: 2023/6/24
 :Content: python课期末大作业，通过一个简单的系统查看前三次作业
 """
-from finalwork.server import main_server
+from server import main_server
 
 if __name__ == '__main__':
     print("<--欢迎来到hzn的python作业系统-->")
