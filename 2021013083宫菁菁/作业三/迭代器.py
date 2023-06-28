c9fc1e851eacd9447df64421537e7b77ca52db3a
Gjj2021013083 <126454859+Gjj2021013083@users.noreply.github.com>
2023-06-28 14:55:43 +0800

    Add files via upload

---
+++ 2021013083宫菁菁/作业三/迭代器.py
@@ -0,0 +1,35 @@
+import requests
+from collections.abc import Iterable,Iterator
+
+def seach(city, list):
+    for key in list:
+        if key[0] == city:
+            return key
+    return 0
+
+class WeatherIterator(Iterator):
+    def __init__(self,city):
+        self.cities = city
+        self.index = 0
+    def getWeather(self, city):
+        r = requests.get("http://www.nmc.cn/dataservice/weather/map/ALL/day2.json?t=1685701082502&_=1685701063671")
+        data = r.json()['data']
+        weather = seach(city, data)
+        return '%s: 最高温【%s】 ,最低温【%s】 ' %(city, weather[8], weather[13])
+    def __next__(self):
+        if self.index == len(self.cities):
+            raise StopIteration
+        city = self.cities[self.index]
+        self.index +=1
+        return self.getWeather(city)
+
+class WeatherIterable(Iterable):
+    def __init__(self,cities):
+        self.cities = cities
+    def __iter__(self):
+        return WeatherIterator(self.cities)
+
+if __name__ == '__main__':
+    city = [u'北京',u'长春']
+    for weather in WeatherIterable(city):
+        print(weather)
