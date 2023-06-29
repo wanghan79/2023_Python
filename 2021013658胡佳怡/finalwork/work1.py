7aa180368a8e1a5efe5521fae602a806764d2538
Ye666666 <136467269+Ye666666@users.noreply.github.com>
2023-06-29 22:27:59 +0800

    Update work1.py

--- 2021013658胡佳怡/finalwork/work1.py
+++ 2021013658胡佳怡/finalwork/work1.py
@@ -3,9 +3,6 @@ import random
 import string
 
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 def dataSampling(**kwargs):
     result=list()
     for item in range(kwargs['num']):
