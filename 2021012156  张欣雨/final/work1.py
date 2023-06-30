3f075b3bcb8c2e1a5d602fc438fd4887fa1d3a02
2021012156 <136557813+2021012156@users.noreply.github.com>
2023-06-30 23:54:34 +0800

    Update work1.py

--- 2021012156  张欣雨/final/work1.py
+++ 2021012156  张欣雨/final/work1.py
@@ -1,31 +1,50 @@
 import random
-import string
-def structDataSampling(**kwargs):
-    result = list()
-    for index in range(0,kwargs["num"]):
-        element = list()
-        for key, value in kwargs['struct'].items():
-            if value['datatype'] == 'int':
-                it = iter(value['datarange'])
-                tmp = random.randint(next(it), next(it))
-            elif value['datatype'] == 'float':
-                it = iter(value['datarange'])
-                tmp = random.uniform(next(it), next(it))
-            elif value['datatype'] == 'str':
-                tmp = ''.join(random.SystemRandom().choice(value['datarange'] )for _ in range(value['len']))
-            else:
-                continue
-            element.append(tmp)
-        result.append(element)
-    return result
+def dataSampling(**kwargs):
+    result = []
+    for key, value in kwargs.items():
+        if isinstance(value, dict):
+            for k, v in value.items():
+                if k == 'type' and v in ['int', 'float', 'str']:
+                    dataType = v
+                elif k == 'size':
+                    size = v
+                elif k == 'range':
+                    dataRange = v
+        else:
+            raise ValueError(f"Invalid input: {key}={value}")
 
-if __name__ == '__main__':
-    try:
-        demo = structDataSampling(num=3, struct={'data1': {'datatype': 'int', 'datarange': [0, 100]},
-                                                 'data2': {'datatype': 'int', 'datarange': [0, 100]},
-                                                 'data3': {'datatype': 'str', 'datarange': ['a','b','c'],'len':5}})
-        print(demo)
-    except (TypeError, KeyError):
-        print("请输入正确的参数，格式为:num=整数, struct={'data1':{'datatype':'int or float', 'datarange':[0,100]}")
-    else:
-        print('程序执行成功')
+    if dataType == 'int':
+        result = [random.randint(*dataRange) for _ in range(size)]
+    elif dataType == 'float':
+        result = [random.uniform(*dataRange) for _ in range(size)]
+    elif dataType == 'str':
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        result = [random.choice(chars) for _ in range(size)]
+    return result
+def sum_avg_decorator(*args):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            data = func(*args, **kwargs)
+
+            if 'SUM' in args and 'AVG' in args:
+                return sum(data), sum(data) / len(data)
+            elif 'SUM' in args:
+                return sum(data)
+            elif 'AVG' in args:
+                return sum(data) / len(data)
+            else:
+                return data
+        return wrapper
+    return decorator
+@sum_avg_decorator() 
+def generate_random_data_1(**kwargs):
+    return dataSampling(**kwargs)
+@sum_avg_decorator('SUM', 'AVG') 
+def generate_random_data_2(**kwargs):
+    return dataSampling(**kwargs)
+@sum_avg_decorator('SUM')  
+def generate_random_data_3(**kwargs):
+    return dataSampling(**kwargs)
+@sum_avg_decorator('AVG') 
+def generate_random_data_4(**kwargs):
+    return dataSampling(**kwargs)
