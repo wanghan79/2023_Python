9a68e016d3f1f59259ee2a75b7cff5d74eb2d024
strawberries-and-cherries <136545394+strawberries-and-cherries@users.noreply.github.com>
2023-06-30 17:44:48 +0800

    Add files via upload

--- 2021011949刘佳惠/final work/final.py
+++ 2021011949刘佳惠/final work/final.py
@@ -1,1 +1,23 @@
+import randomGeneration
+import decorator
+import weather
 
+def show():
+    print("请输入数字1,2,3来选择查看作业1,2,3，输入数字0退出程序")
+    while True:
+        x = int(input())
+        if x == 1:
+            randomGeneration.main1()
+        elif x == 2:
+            decorator.main2()
+        elif x == 3:
+            weather.main3()
+        elif x == 0:
+            print('谢谢使用')
+            break
+        else:
+            print('输入数字为1,2,3,请检查输入！')
+
+
+if __name__ == '__main__':
+    show()
