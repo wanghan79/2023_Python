09f1d6ba9ed3369e6c4a810985ab7216bf33d76e
lingxiang123 <106960144+lingxiang123@users.noreply.github.com>
2023-06-30 23:36:36 +0800

    Update SumAverage.py

--- 2021012686凌湘/work2/SumAverage.py
+++ 2021012686凌湘/work2/SumAverage.py
@@ -11,22 +11,22 @@ def dataProcess(*decorator_args):
                 if isinstance(item, int) or isinstance(item, float):
                     continue
                 else:
-                    raise TypeError("数据格式是非数值，不能参与运算！！请修改结构体传参格式！")
+                    raise TypeError("数据格式错误！")
             total = sum(result)
             average = total / len(result)
             for dec_arg in decorator_args:
                 if dec_arg == "avg":
-                    print("生成的随机列表为：\n", temp)
+                    print("随机列表为：\n", temp)
                     print("avg；\n", average)
                     return average
                 if dec_arg == "sum":
-                    print("生成的随机列表为：\n", temp)
+                    print("随机列表为：\n", temp)
                     print("sum：\n", total)
                     return total
                 elif dec_arg == "all":
-                    print("生成的随机列表为：\n", temp)
-                    print("列表数据求和 sum：\n", total)
-                    print("列表数据均值 avg：\n", average)
+                    print("随机列表为：\n", temp)
+                    print("求和 sum：\n", total)
+                    print("求均值 avg：\n", average)
                     obj = {
                         "result": temp,
                         "total": total,
@@ -42,11 +42,10 @@ def run():
         int: {'datarange': ([1, 100], [1, 100])},
         float: {'datarange': ((0.0, 10.0),)},
     }
-    print("选择修饰器实现的功能：")
-    print("输入 avg 代表对随机数进行求平均值")
-    print("输入 sum 代表对随机数进行求和")
-    print("输入 all 代表呈现随机数组并显示求和与平均值的结果")
-    choose_type = input("输入你的选择： ")
+    print("输入 avg 对随机数求均值")
+    print("输入 sum 对随机数求和")
+    print("输入 all 显示随机数组以及求和与均值的结果")
+    choose_type = input("请输入： ")
     @dataProcess(choose_type)
     def structDataSampling(num, struct):
         return fun_StructRandom(num, struct)
