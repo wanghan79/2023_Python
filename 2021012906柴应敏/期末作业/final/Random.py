1e0468a27dbcda9042b754e46335a504b46a8fab
1820278645 <136249269+1820278645@users.noreply.github.com>
2023-06-30 15:57:46 +0800

    Add files via upload

---
+++ 2021012906柴应敏/期末作业/final/Random.py
@@ -0,0 +1,32 @@
+import random
+import string
+def DataSampling(**kwargs):
+    result = list()
+    for item in range(kwargs['num']):
+        element = list()
+        for key, value in kwargs['struct'].items():
+            if value['datatype'] == 'int':
+                it = iter(value['datarange'])
+                tmp = random.randint(next(it), next(it))
+            elif value['datatype'] == 'float':
+                it = iter(value['datarange'])
+                tmp = random.uniform(next(it), next(it))
+            elif value['datatype'] == 'str':
+                tmp = ''.join(random.SystemRandom().choice(value['datarange']) for _ in range(value['len']))
+            else:
+                break
+            element.append(tmp)
+        result.append(element)
+    return result
+
+if __name__ == '__main__':
+    hw01 = {"num": 5,
+            "struct": {
+                "data1":
+                    {"datatype": 'int', "datarange": [0, 100]},
+                "data2":
+                    {"datatype": 'float', "datarange": [0, 100]},
+                "data3":
+                    {"datatype": 'str', "datarange": string.ascii_uppercase, "len": 5}}
+            }
+    print(DataSampling(**hw01))
