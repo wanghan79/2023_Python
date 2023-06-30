18bd3c6807796b61a0d4c3d1b53921877f48d3d3
qingxz09 <136215829+qingxz09@users.noreply.github.com>
2023-06-30 19:24:37 +0800

    Update main.py

--- 2021012157吴冠颖/final_hmwk/main.py
+++ 2021012157吴冠颖/final_hmwk/main.py
@@ -1,1 +1,18 @@
+import dataSampling
+import data_sum_avg
+import weather
+def main():
+    while True:
+        print(" Enter a number : 1 is hmwk1 , 2 is hmwk2 , 3 is hmwk3 , others are exit")
+        a = input("请输入：")
+        if a == '1':
+            datasampling.DataSampling_res()
+        elif a == '2':
+            data_sum_avg.cal_res()
+        elif a == '3':
+            weather.putWeather()
+        else:
+            break
 
+if __name__=='__main__':
+    main()
